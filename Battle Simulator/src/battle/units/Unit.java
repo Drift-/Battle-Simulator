c34e8205b121a3e5422a4b360a05468623567478
Drift- <dustinkrise@gmail.com>
2015-02-15 18:11:57 -0500

    Power Based on Health
    Implemented system where power of units is based on its health.

--- Battle Simulator/src/battle/units/Unit.java
+++ Battle Simulator/src/battle/units/Unit.java
@@ -17,6 +17,7 @@ public class Unit {
 	
 	public Unit (UnitType type, int team) {
 		this.type = type.type;
+		this.team = team;
 		name = type.name;
 		armour = type.armour;
 		m_speed = type.m_speed;
@@ -41,7 +42,6 @@ public class Unit {
 	private void attack() {
 		//init defender variable (this stores which enemy unit the current unit is attacking)
 		Unit defender = null;
-		
 		//checks team of current unit (won't be needed in later stages)
 		if (team == 0) {
 			//init variables which store the current choice for who the unit is going to attack
@@ -51,7 +51,7 @@ public class Unit {
 				if (type.equals("Ranged")) {
 					//init and assign value to power variable which stores power of possible defender
 					int power = 0;
-					power = u.melee_power;
+					power = (int)(u.melee_power * ((double)u.health/(double)u.fullhealth));
 					//checks if possible defender's power is higher than previous defender choice
 					if (power > defender_power) {
 						//removes current defender choice and adds new one
@@ -63,11 +63,11 @@ public class Unit {
 					double r = 0;
 					if (u.type.equals("Ranged")) {
 						//if possible defender is a ranged unit, calculate
-						r = (double)(u.ranged_power)/(double)fullhealth;
-						r = r/0.125;
+						r = ((double)(u.ranged_power) * ((double)u.health/(double)u.fullhealth))/(double)fullhealth;
+						r = r/0.1;
 					} else if (u.type.equals("Melee")) {
 						//if possible defender is a melee unit, find ratio of current units melee power to possible defender's melee power.
-						r = ((double)melee_power/(double)u.melee_power);
+						r = (((double)melee_power * ((double)health/(double)fullhealth)))/((double)(u.melee_power * ((double)u.health/(double)u.fullhealth)));
 						//modify and round the ratio.
 						r = ((r / 0.25));
 						r = Math.round(r);
@@ -110,7 +110,7 @@ public class Unit {
 				if (type.equals("Ranged")) {
 					//init and assign value to power variable which stores power of possible defender
 					int power = 0;
-					power = u.melee_power;
+					power = (int)(u.melee_power * ((double)u.health/(double)u.fullhealth));
 					//checks if possible defender's power is higher than previous defender choice
 					if (power > defender_power) {
 						//removes current defender choice and adds new one
@@ -122,17 +122,18 @@ public class Unit {
 					double r = 0;
 					if (u.type.equals("Ranged")) {
 						//if possible defender is a ranged unit, calculate
-						r = (double)(u.ranged_power)/(double)fullhealth;
-						r = r/0.125;
+						r = ((double)(u.ranged_power) * ((double)u.health/(double)u.fullhealth))/(double)fullhealth;
+						r = r/0.1;
 					} else if (u.type.equals("Melee")) {
 						//if possible defender is a melee unit, find ratio of current units melee power to possible defender's melee power.
-						r = ((double)melee_power/(double)u.melee_power);
+						r = (((double)melee_power * ((double)health/(double)fullhealth)))/((double)(u.melee_power * ((double)u.health/(double)u.fullhealth)));
+						System.out.println(r + ":" + health);
 						//modify and round the ratio.
 						r = ((r / 0.25));
 						r = Math.round(r);
 						//checks whether or not unmodified ratio is greater than 4.
 						if (r > 4) {
-							//adjusts ratin with health of possible defender
+							//adjusts ratio with health of possible defender
 							r += 1 - (int)(100*((double)u.health/(double)u.fullhealth)/51);
 						} else {
 							//adjusts ratio with health of possible defender
@@ -171,7 +172,7 @@ public class Unit {
 		//checks classification of attacking unit
 		if (type.equals("Melee")) {
 			//if possible defender is a melee unit, find ratio of current units melee power to possible defender's melee power.
-			double r = ((double)melee_power/(double)defender.melee_power);
+			double r = ((double)(melee_power * ((double)health/(double)fullhealth))/(double)(defender.melee_power * ((double)defender.health/(double)defender.fullhealth)));
 			//init variables which track the min and max damage that can be dealt by the defender and the attacker
 			double y_min_d, y_max_d, t_min_d, t_max_d;
 			
@@ -208,8 +209,8 @@ public class Unit {
 
 		} else if (type.equals("Ranged")) {
 			//if unit is a ranged unit, calculate the damage deal and then deal it.
-			double y_min_d = (4/5) * ranged_power;
-			double y_max_d = (6/5) * ranged_power;
+			double y_min_d = (4/5) * ranged_power * ((double)health/(double)fullhealth);
+			double y_max_d = (6/5) * ranged_power * ((double)health/(double)fullhealth);
 			double y_d = y_min_d + (y_max_d - y_min_d) * ran.nextDouble();
 			defender.health -= y_d;
 			//System.out.println("Team " + team + " " + name + " attacked " + defender.name + " for " + (int)y_d + " damage.");
